bb8f389be088135b59d784af25f690dd12d680dd
fdez2002 <fdez2002@gmail.com>
2022-10-02 12:55:53 +0200

    Cabio de int a double

--- app/src/main/java/com/example/radiobuttonoperaciones/MainActivity.java
+++ app/src/main/java/com/example/radiobuttonoperaciones/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Inicializamos los componentes
         numero_uno = (EditText) findViewById(R.id.editTextNumber_1);
         numero_dos = (EditText) findViewById(R.id.editTextNumber_2);
         rb_suma = (RadioButton) this.findViewById(R.id.radioButton_sumar);
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rb_division = (RadioButton) findViewById(R.id.radioButton_dividir);
         tx_resultado = (TextView) findViewById(R.id.textView_resultado);
 
+        //Asignamos los distintos listener
         rb_suma.setOnClickListener(this);
         rb_resta.setOnClickListener(this);
         rb_multiplicacion.setOnClickListener(this);
@@ -41,28 +43,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
-    //Supervisamos cuando el usuario seleciona algunos de los componentes
+
+    //Controlamos que control ha sido seleccionado
     @Override
     public void onClick(View view){
-        int res = 0;
-        int num1 = Integer.parseInt(numero_uno.getText().toString());
-        int num2 = Integer.parseInt(numero_dos.getText().toString());
-        /*
+        double res = 0;
+        double num1 = Integer.parseInt(numero_uno.getText().toString());
+        double num2 = Integer.parseInt(numero_dos.getText().toString());
 
-        if(view.getId() == R.id.radioButton_sumar){
-            res = num1 + num2;
-        }else if(view.getId() ==  R.id.radioButton_restar){
-            res = num1 - num2;
-        }else if (view.getId() == R.id.radioButton_multiplicar){
-            res = num1 * num2;
-        }else if(view.getId() == R.id.radioButton_dividir){
-
-        }
-
-         */
-
-
-        switch (view.getId()){
+        //Cada boton tiene un valor unico en formato entero
+        switch (view.getId()){//El metodo getid() nos devuelve el control que fue seleccionado en formato de entero
             case R.id.radioButton_sumar:
                 res = num1 + num2;
                 break;
@@ -73,13 +63,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 res = num1 * num2;
                 break;
             case R.id.radioButton_dividir:
-                if(num2 == 0){
+                if(num2 == 0){//Si num2 es igual a 0 nos devuelve un mensaje diciendo que no s epuede dividir por 0
                     Toast.makeText(this, "No se puede dividir por 0", Toast.LENGTH_SHORT).show();
                 }else{
                     res = num1 / num2;
                 }
                 break;
         }
+        //Comprobamos de nuevo si num2 = 0 y si esta seleccionado la opcion dividir, si es asi resultado dara un mensaje de error
+        //Si no hara la operación sin ningun tipo de error
         if (view.getId() == R.id.radioButton_dividir && num2 == 0){
             tx_resultado.setText(("No se puede dividir por 0"));
         }else{
